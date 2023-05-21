bd7a5f1948ca1b5886a6fd8b42f0d6c96e2076a4
Istvan Gombos <gmbs.istvan@gmail.com>
2023-05-21 16:01:40 +0200

    spring io+git repo+clone+push

---
+++ src/main/java/hu/nye/progkor/kvweb/KvWebApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.kvweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/progkor/kvweb/KvWebApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.kvweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
